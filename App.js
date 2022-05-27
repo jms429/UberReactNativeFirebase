5936903e6ce08db89288daeac1d7a1f4590a9539
Jason L Frederick <jms429@gmail.com>
2022-05-27 17:28:13 +0000

    Finished Catagory and restauraunt items (pre-live data)

--- App.js
+++ App.js
@@ -6,7 +6,6 @@ import Home from './screens/Home';
 export default function App() {
   return (
     <View>
-  
       <Home />
     </View>
   );
--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,26 @@
-import { View, Text, SafeAreaView} from 'react-native'
+import { StatusBar, View, Text, SafeAreaView, ScrollView,} from 'react-native'
 import React from 'react'
 import HeaderTab from '../components/HeaderTab'
 // import { SafeAreaView } from 'react-native-safe-area-context'
 import SearchBar from '../components/SearchBar'
+import Catagories from '../components/Catagories'
+import RestaurantItems from '../components/RestaurantItems'
+// import { ScrollView } from 'react-native-web'
 
 export default function Home() {
   return (
-    <SafeAreaView style={{backgroundColor: "#eee" }}>
+    <>
+    <StatusBar backgroundColor="black" barStyle="white-content" />
+    <SafeAreaView style={{backgroundColor: "#eee", height: "100%"}}>
         <View style={{ backgroundColor:"white", padding: 15}}>
             <HeaderTab />
             <SearchBar />
         </View>
+        <ScrollView showsVerticalScrollIndicator={false} >
+            <Catagories />
+            <RestaurantItems />
+        </ScrollView>
     </SafeAreaView>
-
+    </>
   )
 }
